47336d3281e67d5c372840e9e64154a4aef6c043
dojo2023-masui <130331034+dojo2023-masui@users.noreply.github.com>
2023-06-12 16:43:02 +0900

    add_file(2)
    最新だよ～！

--- src/src/model/Masui.java
+++ src/src/model/Masui.java
@@ -1,13 +1,3 @@
 package model;
 
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 
-/**
- * Servlet implementation class Masui
- */
-@WebServlet("/Masui")
-public class Masui extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-
-}
