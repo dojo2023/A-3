efd73720906ae9dc2e4e744272d4b7d7a8363a8d
dojo2023-masui <130331034+dojo2023-masui@users.noreply.github.com>
2023-06-19 13:49:53 +0900

    change_file
    registerservlet.java 変更したよ～！

--- src/src/servlet/RegisterServlet.java
+++ src/src/servlet/RegisterServlet.java
@@ -4,14 +4,21 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import dao.RegisterDao;
+import model.Registers;
+
 
 /**
  * Servlet implementation class RegisterServlet
  */
+@MultipartConfig(location = "C:\\pleiades\\workspace\\Nyample\\WebContent\\images") // アップロードファイルの一時的な保存先
 @WebServlet("/RegisterServlet")
 public class RegisterServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -32,9 +39,75 @@ public class RegisterServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 
+		request.setCharacterEncoding("UTF-8");
+		//主キー
+		String ID= request.getParameter("ID");
+		//アイテムカテゴリ
+		String OUTER= request.getParameter("OUTER");
+		String TOPS= request.getParameter("TOPS");
+		String BOTTOMS= request.getParameter("BOTTOMS");
+		String DRESS= request.getParameter("DRESS");
+		String SOCKS= request.getParameter("SOCKS");
+		String SHOSE= request.getParameter("SHOSE");
+		String ACCE= request.getParameter("ACCE");
+		//サブカテゴリ
 
-		//なにするのかをどんどん書いていく(コメントアウト)
+		//タグ
+		String SPRING= request.getParameter("SPRING");
+		String SUMMER= request.getParameter("SUMMER");
+		String AUTUM= request.getParameter("AUTUM");
+		String WINTER= request.getParameter("WINTER");
+		String CUTE= request.getParameter("CUTE");
+		String CASUAL= request.getParameter("CASUAL");
+		String SIMPLE= request.getParameter("SIMPLE");
+		String STRIPE= request.getParameter("STRIPE");
+		String CHECK= request.getParameter("CHECK");
+		String DOT= request.getParameter("DOT");
+		String BEAUTY= request.getParameter("BEAUTY");
+		String MODE= request.getParameter("MODE");
+		String NATURAL= request.getParameter("NATURAL");
+		String CONSERVA= request.getParameter("CONSERVA");
+		String COOL= request.getParameter("COOL");
+		String LOWHEIGHT= request.getParameter("LOWHEIGHT");
+		String MENS= request.getParameter("MENS");
+		String LOWPRICE= request.getParameter("LOWPRICE");
+		String MONOTONE= request.getParameter("MONOTONE");
+		String SKEWAVWINTER= request.getParameter("WINTER");
+		String SKESTRAIGHT= request.getParameter("SKESTRAIGHT");
+		String SKENATURAL= request.getParameter("SKENATURAL");
+		String REPEAT= request.getParameter("WINTER");
+		String IMG = request.getParameter("IMG ");
+
+		//画像アップロード
+		Part part = request.getPart("IMAGE"); // getPartで取得
+		String image = this.getFileName(part);
+		request.setAttribute("image", image);
+		// サーバの指定のファイルパスへファイルを保存
+        //場所はクラス名↑の上に指定してある
+		part.write(image);
+
+		RegisterDao rDao = new RegisterDao();
+		if (rDao.insert(new Registers())) {	// 登録成功
+		request.setAttribute("result",
+		new Registers());
+		}
+		else {												// 登録失敗
+		request.setAttribute("result",
+		new Registers());
+		}
+
+		//ファイルの名前を取得してくる
+		private String getFileName(Part part) {
+	        String name = null;
+	        for (String dispotion : part.getHeader("Content-Disposition").split(";")) {
+	            if (dispotion.trim().startsWith("filename")) {
+	                name = dispotion.substring(dispotion.indexOf("=") + 1).replace("\"", "").trim();
+	                name = name.substring(name.lastIndexOf("\\") + 1);
+	                break;
+	            }
+	        }		// TODO 自動生成されたメソッド・スタブ
+			return name;
+		}
 
 	}
-
 }
