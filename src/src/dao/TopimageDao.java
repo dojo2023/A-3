24cddbf9d4d6ede4617f6327607833b8c4de071a
dojo2023-yamato <yamatoshinnosukeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-19 16:16:38 +0900

    change_file
    model.Topimages.java
    dao.TopimageDao.java
    変更です。

--- src/src/dao/TopimageDao.java
+++ src/src/dao/TopimageDao.java
@@ -5,4 +5,67 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Topimages;
+
+public class TopimageDao {
+
+    private Connection conn;
+
+    public List<Topimages> getTopImages() throws SQLException {
+        List<Topimages> imageList = new ArrayList<>();
+        try {
+
+        	 // JDBCドライバを読み込む
+            Class.forName("org.h2.Driver");
+
+            // データベースに接続する
+            conn = DriverManager.getConnection("jdbc:h2:file:C:\\dojo6\\data", "sa", "sa");
+
+
+	        // SQL文を準備する
+	        String sql = "SELECT * FROM TOP LIMIT 5" ;
+	        PreparedStatement pStmt = conn.prepareStatement(sql);
+
+	        // SQL文を完成させる
+	//        pStmt.setString(1, ID);
+	//        pStmt.setString(2, IMG);  //←画像なのにString型?
+
+	        // SQL文の実行
+	        ResultSet rs = pStmt.executeQuery();
+	        while (rs.next()) {
+	            Topimages e = new Topimages();
+	            e.setTopid(rs.getInt("TOPID"));
+	            e.setTopimg(rs.getString("TOPIMG"));
+	            imageList.add(e);
+	        }
+
+        }
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+	    return imageList;
+    }
+}
+
+
+
+
 
--- src/src/model/Topimages.java
+++ src/src/model/Topimages.java
@@ -1,1 +1,34 @@
 package model;
+import java.io.Serializable;
+
+public class Topimages implements Serializable{
+
+	private int topid;
+	private String topimg;
+
+	public Topimages(int topid, String topimg) {
+		super();
+		this.topid = topid;
+		this.topimg = topimg;
+   }
+
+	public Topimages(){
+		super();
+		this.topid= 0;
+		this.topimg = "";
+
+	}
+	public int getTopid() {
+		return topid;
+	}
+	public String getTopimg() {
+		return topimg;
+	}
+	public void setTopid(int topid) {
+		this.topid = topid;
+	}
+	public void setTopimg(String topimg) {
+		this.topimg = topimg;
+	}
+
+}
