9b28e55e9eaf3272c5522973abe3d3fe3a27a923
dojo2023-yamato <yamatoshinnosukeplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-20 09:39:04 +0900

    change_TopimageDao
    トップ画面のDAOの変更です！

--- src/src/dao/TopimageDao.java
+++ src/src/dao/TopimageDao.java
@@ -12,7 +12,7 @@ import model.Topimages;
 
 public class TopimageDao {
 
-    private Connection conn;
+  Connection conn = null;
 
     public List<Topimages> getTopImages() throws SQLException {
         List<Topimages> imageList = new ArrayList<>();
@@ -26,12 +26,12 @@ public class TopimageDao {
 
 
 	        // SQL文を準備する
-	        String sql = "SELECT * FROM TOP LIMIT 5" ;
+	        String sql = "SELECT * FROM TOP LIMIT 5" ; //LIMIT 5を消せば全検索
 	        PreparedStatement pStmt = conn.prepareStatement(sql);
 
 	        // SQL文を完成させる
 	//        pStmt.setString(1, ID);
-	//        pStmt.setString(2, IMG);  //←画像なのにString型?
+	//        pStmt.setString(2, IMG);
 
 	        // SQL文の実行
 	        ResultSet rs = pStmt.executeQuery();
@@ -60,12 +60,146 @@ public class TopimageDao {
 				}
 			}
 		}
-
+        //結果を返す
 	    return imageList;
     }
+
+             //INSERT
+        //引数imageで指定されたレコードを登録し、成功したらtrueを返す
+   public boolean insert(Topimages image) {
+	  Connection conn = null;
+	  boolean result = false;
+
+	  try {
+		// JDBCドライバを読み込む
+		Class.forName("org.h2.Driver");
+
+		// データベースに接続する
+		conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/TRex", "sa", "sa");
+
+		// SQL文を準備する
+		String sql = "INSERT INTO TOP (TOPID, TOPIMG) VALUES (?, ?)";
+        PreparedStatement pStmt = conn.prepareStatement(sql);
+
+        //SQL文を完成させる
+        pStmt.setInt(1, image.getTopid());
+        pStmt.setString(2, image.getTopimg());
+
+     // SQL文を実行する
+		if (pStmt.executeUpdate() == 1) {
+			result = true;
+		}
+	}
+	catch (SQLException e) {
+		e.printStackTrace();
+	}
+	catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	}
+	finally {
+		// データベースを切断
+		if (conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 結果を返す
+	return result;
 }
 
+           //UPDATE
+   // 引数imageで指定されたレコードを更新し、成功したらtrueを返す
+	public boolean update(Topimages image) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/dojo6/data/TRex", "sa", "sa");
+
+			// SQL文を準備する
+			String sql = "UPDATE TOP SET TOPIMG = ? WHERE TOPID = ?";
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            //SQL文を完成させる
+            pStmt.setString(1, image.getTopimg());
+            pStmt.setInt(2, image.getTopid());
 
 
+			// SQL文を実行する
+			if (pStmt.executeUpdate() == 1) {
+				result = true;
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		finally {
+			// データベースを切断
+			if (conn != null) {
+				try {
+					conn.close();
+				}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		// 結果を返す
+		return result;
+	}
+
+	//DELATE文
+	// 引数imageで指定されたレコードを削除し、成功したらtrueを返す
+	public boolean delete(Topimages image) {
+		Connection conn = null;
+		boolean result = false;
+
+		try {
+			// JDBCドライバを読み込む
+			Class.forName("org.h2.Driver");
+
+			// データベースに接続する
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/TRex", "sa", "sa");
+			//SQL文を準備する
+			String sql = "DELETE FROM TOP WHERE TOPID = ?"; //TOPからTOPIDが指定された値と一致するレコードを削除
+            PreparedStatement pStmt = conn.prepareStatement(sql);
+            //SQL文を完成させる
+            pStmt.setInt(1, image.getTopid());
+
+            // SQL文を実行する
+            if (pStmt.executeUpdate() == 1) {
+                result = true;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            // データベースを切断
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // 結果を返す
+        return result;
+    }
 
 
+	}
