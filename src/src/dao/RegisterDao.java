aa93985a9f0ceba4a57b9c1c1e369fc625351c1e
dojo2023-noda <nodakannaplusdojo2023@seplus2016.onmicrosoft.com>
2023-06-27 17:26:53 +0900

    change_RegisterDao
    全件検索に性別を追加

--- src/src/dao/RegisterDao.java
+++ src/src/dao/RegisterDao.java
@@ -81,6 +81,7 @@ public class RegisterDao{// 引数paramで検索項目を指定し、検索結�
 						,rs.getString("SKEWAVE"),rs.getString("SKESTRAIGHT"),rs.getString("SKENATURAL"),rs.getString("REPEAT")};
 				poster.setTag(tag);
 				poster.setImg(rs.getString("IMAGE"));
+				poster.setGender(rs.getString("GENDER"));
 				poster.setInsert_date(rs.getDate("INSERT_DATE"));
 				postList.add(poster);
 
